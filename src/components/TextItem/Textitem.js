e5cc4ace2eed03babfb04b283a845474708cc9a9
k2 <trofimchuk.an@gmail.com>
2018-11-24 00:06:10 +0200

    Saving notes in localStorage

--- src/components/TextItem/Textitem.js
+++ src/components/TextItem/Textitem.js
@@ -1,11 +1,43 @@
 import React from "react";
+import "./style.css";
 
 class TextItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleTextChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+  handleNewNoteAdd = () => {
+    let newNote = {
+      id: Date.now(),
+      text: this.state.text,
+      color: "yellow"
+    };
+    this.props.noteAdd(newNote);
+    this.setState({
+      text: ""
+    });
+  };
+
   render() {
     return (
-      <div>
-        <textarea />
-        <button>Add</button>
+      <div className="textAreaWrapper">
+        <textarea
+          className="textarea"
+          placeholder="Enter your note ..."
+          rows={5}
+          onChange={this.handleTextChange}
+        />
+        <button className="btnAdd" onClick={this.handleNewNoteAdd}>
+          Add
+        </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,50 +5,42 @@ import "./index.css";
 import Textarea from "./components/TextItem/Textitem";
 import NotesList from "./components/NotesList/NotesList";
 
-let NodesArray = [
-  {
-    id: 1,
-    text:
-      "Lorem Ipsum - це текст-риба, що використовується в друкарстві та дизайні. Lorem Ipsum є, фактично",
-    color: "red"
-  },
-  {
-    id: 2,
-    text:
-      "Вже давно відомо, що читабельний зміст буде заважати зосередитись людині, яка оцінює композицію сторінки. Сенс використання Lorem Ipsum полягає в тому, що цей текст має більш-менш нормальне розподілення літер на відміну від, наприклад,",
-    color: "yellow"
-  },
-  {
-    id: 3,
-    text:
-      "На відміну від поширеної думки Lorem Ipsum не є випадковим набором літер. Він походить з уривку класичної латинської літератури 45 року до н.е.,",
-    color: "#ffD700"
-  },
-  {
-    id: 4,
-    text:
-      "снує багато варіацій уривків з Lorem Ipsum, але більшість з них зазнала певних змін на кшталт жартівливих вставок або змішування слів,",
-    color: "#ffD700"
-  },
-  {
-    id: 5,
-    text:
-      "На відміну від поширеної думки Lorem Ipsum не є випадковим набором літер. Він походить з уривку класичної латинської літератури 45 року до н.е., тобто має більш як 2000-річну історію. Річард Макклінток, професор латини з коледжу Хемпдін-Сидні, що у Вірджінії, вивчав одне з найменш зрозумілих латинських слів - consectetur - з уривку Lorem Ipsum, і у пошуку цього слова в класичній літературі знайшов безсумнівне джерело. Lorem Ipsum походить з розділів 1.10.32 ",
-    color: "brown"
-  }
-];
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { Notes: NodesArray };
+    this.state = { Notes: [] };
   }
+
+  componentDidMount() {
+    let localNotes = JSON.parse(localStorage.getItem("notes"));
+    if (localNotes) {
+      this.setState({
+        Notes: localNotes
+      });
+    }
+  }
+
+  updateStorage() {
+    let notes = JSON.stringify(this.state.Notes);
+    localStorage.setItem("notes", notes);
+  }
+
+  handleAddNote = newNote => {
+    let newNotes = this.state.Notes.slice();
+    newNotes.unshift(newNote);
+    this.setState(
+      {
+        Notes: newNotes
+      },
+      this.updateStorage
+    );
+  };
 
   render() {
     return (
       <div>
         <h2>ToDoList</h2>
-        <Textarea />
+        <Textarea noteAdd={this.handleAddNote} />
         <NotesList Notes={this.state.Notes} />
       </div>
     );
