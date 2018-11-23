5b201bbe03b8ef7f64567ece81604540dd3f0443
k2 <trofimchuk.an@gmail.com>
2018-11-23 18:22:30 +0200

    Added basic styles

--- src/components/NotesItem/NoteItem.js
+++ src/components/NotesItem/NoteItem.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./style.css";
 
 class NoteItem extends React.Component {
   render() {
     let bgk = { background: this.props.color };
-    return <div style={bgk}>{this.props.text}</div>;
+    return (
+      <div style={bgk} className="note">
+        {this.props.text}
+      </div>
+    );
   }
 }
 
