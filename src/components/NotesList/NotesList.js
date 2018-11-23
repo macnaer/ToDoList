90738ba2fba34aec9ba6b7493ce58c186e993784
k2 <trofimchuk.an@gmail.com>
2018-11-23 22:59:12 +0200

    Included masonry.js

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -1,11 +1,21 @@
 import React from "react";
 import NoteItem from "../NotesItem/NoteItem";
+import Masonry from "react-mason";
 
 class NotesList extends React.Component {
+  componentDidMount() {
+    let grid = this.refs.grid;
+    this.msnry = new Masonry(grid, {
+      itemSelector: ".note",
+      columnWidth: 200,
+      gutter: 10
+    });
+  }
+
   render() {
     console.log(this.props);
     return (
-      <div>
+      <div className="notesList" ref="grid">
         {this.props.Notes.map(note => {
           return <NoteItem key={note.id} text={note.text} color={note.color} />;
         })}
